ceaec263aec3f01c5c47b50f02b96ea253bc3a92
xandervdh <xander-v.d.h@hotmail.com>
2020-10-19 19:16:50 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types=1);
+
+//include all your model files here
+
+//include all your controllers here
+
+//you could write a simple IF here based on some $_GET or $_POST vars, to choose your controller
+//this file should never be more than 20 lines of code!
---
+++ view/homepage.php
@@ -0,0 +1,8 @@
+<?php
+declare(strict_types=1);
+
+require 'includes/header.php';
+
+//code
+
+require 'includes/footer.php';
