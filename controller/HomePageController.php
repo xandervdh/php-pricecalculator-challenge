7c3235e0330bbbc84beccebbd04645536870593d
xandervdh <xander-v.d.h@hotmail.com>
2020-10-20 10:11:41 +0200

    added the controller for the homepage

---
+++ controller/HomePageController.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types = 1);
+
+class HomepageController
+{
+    public function render()
+    {
+        $customers = new Customer();
+
+
+
+        require '../view/homepage.php';
+    }
+}
