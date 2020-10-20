440b67de34d6a25ac8937c9884967fa1f530bc6a
Beardificent <cis.magito@gmail.com>
2020-10-20 13:51:36 +0200

    removed redundant code for Xander

--- model/Customers.php
+++ model/Customers.php
@@ -4,26 +4,16 @@
 class Customers
 {
     private $pdo;
-    /* @var Customer[]*/
     private array $customers = [];
 
     public function __construct ($pdo)
     {
         $this->pdo = $pdo;
-        //$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $handle = $pdo->prepare("SELECT id, firstname, lastname FROM customer");
-        $handle->execute();
-        $users = $handle->fetchAll();
-        var_dump($users);
-        $customerCount = count($this->customers);
         $getAllCustomers = $this->getAllCustomers();
         foreach ($getAllCustomers as $row){
             array_push($this->customers, new Client($row['firstname'], $row['lastname']));
         }
 
-var_dump($this->customers);
-
-       // array_push($this->customers, new Customer($this->firstname,$this->lastname));
 
     }
 
@@ -33,9 +23,6 @@ var_dump($this->customers);
 
     }
 
-    /**
-     * @return Customer[]
-     */
     public function getCustomers(): array
     {
         return $this->customers;
