8393c5521d05301604b77b7398b107d0fc4c867f
Beardificent <cis.magito@gmail.com>
2020-10-21 09:44:22 +0200

    removed select * and replaced it with select firstname, lastname, ...

--- model/Customers.php
+++ model/Customers.php
@@ -5,23 +5,41 @@ class Customers
 {
     private $pdo;
     private array $customers = [];
+    private array $customerGroups = [];
 
     public function __construct ($pdo)
     {
         $this->pdo = $pdo;
         $getAllCustomers = $this->getAllCustomers();
         foreach ($getAllCustomers as $row){
-            array_push($this->customers, new Client($row['firstname'], $row['lastname'], $row['id']));
+            array_push($this->customers, new Client($row['firstname'], $row['lastname']));
         }
 
 
     }
 
     function getAllCustomers() {
-        return $this->pdo->query('SELECT * FROM customer');
+        return $this->pdo->query('SELECT firstname, lastname, group_id, variable_discount, fixed_discount FROM customer ORDER BY lastname');
 
 
     }
+
+  /* public function getGroups()
+    {
+        $groupId = $customer['group_id'];
+
+        do{
+            $handler = $this->pdo->prepare('SELECT * FROM customer_group WHERE id = :id');
+            $handler->bindValue(':id', $groupId);
+            $handler->execute();
+            $customerGroup = $handler->fetch();
+            array_push($this->customerGroups, $customerGroup);
+
+            $groupId = $customerGroup['parent_id'];
+        } while($groupId != null);
+
+    }
+  */
 
     public function getCustomers(): array
     {
