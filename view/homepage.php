2a0c24ae12ef1af02c1ed825f6d1926e89d65686
Beardificent <cis.magito@gmail.com>
2020-10-21 12:45:05 +0200

    adding styling to page and added /100 behind productprice in homepage.php on line 24

--- view/homepage.php
+++ view/homepage.php
@@ -4,10 +4,11 @@ declare(strict_types=1);
 require 'includes/header.php';
 ?>
 <?php echo $success; ?>
+<div class="form-container">
     <form method="post">
         <label for="client">Choose a client:</label>
-        <select id="client" name="client">
-            <option value="empty" selected>- Choose option -</option>
+        <select class='option-box' id="client" name="client">
+                <option value="empty" selected>- Choose option -</option>
             <?php
             foreach ($clients as $client) {
                 echo '<option value="' . $client->getFirstname() . ' ' . $client->getLastname() . '">' . $client->getFirstname() . ' ' . $client->getLastname() . '</option>';
@@ -15,16 +16,17 @@ require 'includes/header.php';
             ?>
         </select><br>
         <label for="product">Choose a product:</label>
-        <select id="product" name="product">
+        <select class='option-box' id="product" name="product">
             <option value="empty" selected>- Choose option -</option>
             <?php
             foreach ($productsArray as $product) {
-                echo '<option value="' . $product->getProductname() . '">' . $product->getProductname() . ' - €' . $product->getProductprice() . '</option>';
+                //productPrice()/100
+                echo '<option value="' . $product->getProductname() . '">' . $product->getProductname() . ' - €' . $product->getProductprice()/100 . '</option>';
             }
             ?>
         </select><br>
         <input type="submit" value="Calculate" class="btn btn-primary">
     </form>
-
+</div>
 
 <?php require 'includes/footer.php'; ?>
