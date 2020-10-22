e7bf4d6ffabc4235c11978790192cdc448e06826
xandervdh <xander-v.d.h@hotmail.com>
2020-10-22 09:44:10 +0200

    Update homepage.php

--- view/homepage.php
+++ view/homepage.php
@@ -7,18 +7,6 @@ require 'includes/header.php';
 
     <div class="form-container">
         <form method="post">
-<<<<<<< HEAD
-            <!--<label for="client">Choose user:</label>
-            <select class='option-box' id="client" name="client">
-                <option value="empty" selected>- Choose option -</option>
-                <?php
-                foreach ($clients as $client) {
-                    echo '<option value="' . $client->getLastname() . '">' . $client->getFirstname() . ' ' . $client->getLastname() . '</option>';
-                }
-                ?>
-            </select><br>-->
-=======
->>>>>>> c7c3e93619a4137df4c83987e9c05c916df1f680
             <label for="product">Choose a product:</label>
             <select class='option-box' id="product" name="product">
                 <option value="empty" selected>- Choose option -</option>
@@ -29,9 +17,6 @@ require 'includes/header.php';
                 }
                 ?>
             </select><br>
-<<<<<<< HEAD
-            <input type="submit" value="Calculate" class="btn">
-=======
             <label for="quantity">Choose quantity:</label>
             <select class='option-box' id="quantity" name="quantity">
                 <option value="empty" selected>- Choose option -</option>
@@ -50,8 +35,7 @@ require 'includes/header.php';
                 }
                 ?>
             </select><br>
-            <input type="submit" value="Calculate" class="btn btn-primary">
->>>>>>> c7c3e93619a4137df4c83987e9c05c916df1f680
+            <input type="submit" value="Calculate" class="btn">
         </form>
         <div id="echo-success"><?php echo $success; ?></div>
         <div id="result">
