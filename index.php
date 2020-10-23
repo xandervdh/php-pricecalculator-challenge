4ec6126e31d49f3354d638804694a557485235ad
xandervdh <xander-v.d.h@hotmail.com>
2020-10-23 09:46:43 +0200

    added comments

--- index.php
+++ index.php
@@ -1,12 +1,13 @@
 <?php
 declare(strict_types=1);
 
+//if you choose a client show the homepage
 if (isset($_GET['client'])){
     require 'controller/HomePageController.php';
     $controller = new HomepageController();
-} else {
+} else { //else show the login page
     require 'controller/LoginController.php';
     $controller = new LoginController();
 }
-
+//render the view
 $controller->render();
